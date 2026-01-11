309a59cbe95d4f3d3b8a0d47fbc9c06e32b274fb
techslb <techslb@outlook.com>
2026-01-11 17:29:57 -0600

    Create codedump.py

---
+++ codedump.py
@@ -0,0 +1,37 @@
+greeting = "The Red Divide"
+
+print(greeting)
+
+modifiable_stats_list = [
+        ["Health", "Description: Total hit points before your demise. Rest to restore."],
+        ["Stamina", "Description: Total fatigue points. Regenerates between actions." ],
+        ["Firearms", "Description: Effectiveness of lead projectile based weaponry."],
+        ["Augmented Weapons", "Description: Effectiveness of weapons augmented to your Neural Interface Armor."],
+        ["Melee Weapons", "Description: Effectiveness of martial based weaponry."],
+        ["Stealth", "Description: Effectiveness of your ability to remain unseen and unheard."],
+        ["Speech", "Description: Effectiveness of your ability to persuade or intimidate others."],
+        ["Hacking", "Description: Effectiveness of your ability to bio-hack machines and interfaces."],
+        ["Haggling", "Description: Effects the price you purchase and sell items."],
+        ["Repair", "Description: Effectiveness of your ability to repair your weapons and armor."],
+        ["Chemistry", "Description: Effectiveness of your ability to concoct scientific formulas for buffs, health, stamina, and explosives."],
+        ["Explosives," "Description: Effectiveness of your ability with explosives and fire based weaponry."],
+        ["Scavenging", "Description: Effectiveness of your ability to scavenge materials from the environment and enemies."],
+        ["Leadership", "Description: Effectiveness of your ability to command allies in combat and how effective they are."],
+        ["Crafting", "Description: Effectiveness of your ability to craft items from raw materials."]
+
+        ]
+
+unmodifiable_stats_list = [
+        ["Food", "Description: Level of total fullness. Eat to restore."],
+        ["Water", "Description: Level of total hydration. Drink water to restore."],
+        ["Sleep", "Description: Sleep deprivation level"],
+
+        ]
+# name and desc are iterator loops
+# they print the first raw string lines
+for name, desc in modifiable_stats_list:
+  print(f"Skill: {name}")
+  print(desc)
+
+
+        
